018eaf8f17087570478d1ce0af5a41b29ed9cbbb
imane sabir <sabimane1997@gmail.com>
2020-05-09 15:56:33 +0000

    first commit

---
+++ drop/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'drop';
+  mylist=["a","b","c","d","e","f","g","h","i","j"
+,"k","l","m","n","o","p","q","r","s","t"]
+}
